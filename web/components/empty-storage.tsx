7e0aaa7374a617223a526ed0a012e80496fb6e22
iiiusi0n <iiiusi0n@wyvern.pw>
2024-07-29 01:48:55 +0900

    Add Caddyfile for reverse proxy

---
+++ web/components/empty-storage.tsx
@@ -0,0 +1,11 @@
+export function EmptyStorage() {
+  return (
+      <>
+          <div className="border rounded-lg p-6 flex items-center justify-center h-[100%] bg-muted">
+              <div className="text-center space-y-2">
+                  <p className="text-lg font-medium">Create new storage to start</p>
+              </div>
+          </div>
+      </>
+  )
+}
